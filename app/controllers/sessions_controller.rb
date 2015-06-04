8d6eb380f03e52668067ee482c4dfe5270f4a5d5
Julia Becker <jcbecker26@gmail.com>
2015-06-04 12:22:07 -0400

    testing oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,57 +20,75 @@ class SessionsController < ApplicationController
      redirect_to login_path
   end
 
+
+  # Helper method makes the method available to the view (we link to it from the sessions new page)
+  helper_method :code_uri
+
+  FACEBOOK_CODE_URI = 'https://www.facebook.com/dialog/oauth'
+  FACEBOOK_TOKEN_URI = 'https://www.facebook.com/connect/login_success.html#access_token'
+  FACEBOOK_API_URI = 'https://graph.facebook.com/me'
+
+
   def redirect_uri
     root_url[0..-2] + oauth_callback_path
   end
 
+  def code_uri
+    query_params = "?" + URI.encode_www_form({
+      response_type: 'code',
+      client_id:     927367780617811,
+      redirect_uri:  redirect_uri,
+      scope:         'email,user_friends'
+    })
+      FACEBOOK_CODE_URI + query_params
+  end
+
   def request_token
-    response = HTTParty.post "https://www.facebook.com/dialog/oauth", {
+    response = HTTParty.post FACEBOOK_TOKEN_URI, {
       body: {
         :client_id  => 927367780617811,
         :redirect_uri  => redirect_uri,
-      :response_type    => "token"
-      },
-      headers: {
-        'Accept' => 'application/json'
-      },
-      format: :json # parse the response as JSON
+      :response_type => "token"
       }
-    }
+      # headers: {
+      #   'Accept' => 'application/json'
+      # },
+      # format: :json # parse the response as JSON
+      }
     response['access_token']
   end
 
   def get_credentials_with(token)
-    response = HTTParty.get "https://www.facebook.com/connect/login_success.html#
-    access_token", {
+    response = HTTParty.get FACEBOOK_API_URI, {
       headers: {
         'Authorization' => "token #{token}",
         'User-Agent'    => 'request'
       },
       format: :json
+    }
       # Return credentials
       return {
         access_token: token,
-        facebook_id: response['user_id'],
+        facebook_id: response['id'],
         first_name: response['first_name'],
-        last_name: response['last_name'],
-        email: response['email'],
-        picture: response['profile_picture']
+        # last_name: response['last_name'],
+        # email: response['email'],
+        # picture: response['picture']
         # NEED TO ADD FRIENDS
       }
-    }
   end
 
   def log_in_user_by(credentials)
     user = User.find_or_initialize_by({facebook_id: credentials[:user_id]})
 
     if user.new_record?
-      # user.first_name = credentials[:first_name]
+      user.facebook_id = credentials[:facebook_id]
+      user.first_name = credentials[:first_name]
       # user.last_name = credentials[:last_name]
       # user.email = credentials[:email]
-      # user.picture = credentials[:profile_picture]
+      # user.picture = credentials[:picture]
       # # NEED TO ADD FRIENDS
-      # user.save
+      user.save
     end
 
     session[:access_token] = credentials[:token]
